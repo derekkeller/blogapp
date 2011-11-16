92e3ac61cd5b213044451fd9b29f23f6f43d3162
Derek Keller <derekbkeller@gmail.com>
2011-11-15 21:05:28 -0700

    All is working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @micropost_user_ids = current_user.followers.collect {|follower| follower.id}
     
-    @microposts = Micropost.where('user_id = ? OR ?', @micropost_user_ids, current_user.id).order('created_at DESC')
+    @microposts = Micropost.where('user_id = ? OR ?', @micropost_user_ids, current_user.id)
   end
 
   def contact
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ActiveRecord::Base
 
+  default_scope :order => 'created_at DESC'
+
   belongs_to :user
   
 end
